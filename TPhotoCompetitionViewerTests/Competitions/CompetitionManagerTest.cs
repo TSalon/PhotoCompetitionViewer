70ed6e092c82ded1c41a48701375ef05594fd203
Tim Sawyer <tim.git@hotmandarins.co.uk>
2018-10-16 12:22:09 +0100

    CompetitionManager test that works with a defined test competition zip file

---
+++ TPhotoCompetitionViewerTests/Competitions/CompetitionManagerTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TPhotoCompetitionViewer.Competitions;
+using TPhotoCompetitionViewer.Properties;
+
+namespace TPhotoCompetitionViewerTests.Competitions
+{
+    [TestClass]
+    public class CompetitionManagerTest
+    {
+        [TestInitialize]
+        public void Setup()
+        {
+            // Get hold of settings, then change them
+            Settings.Default.Reload();
+            var extractDir = Settings.Default.CompetitionExtractDir;
+            var srcDir = Settings.Default.CompetitionSrcDir;
+
+            Settings.Default.PropertyValues["CompetitionExtractDir"].PropertyValue = "./TestData";
+            Settings.Default.PropertyValues["CompetitionSrcDir"].PropertyValue = "./TestCompetitionZip";
+        }
+
+        [TestMethod]
+        public void TestGetCompetitions()
+        {
+            CompetitionManager competitionMgr = new CompetitionManager();
+            List<string> competitionsList = competitionMgr.GetCompetitions();
+            Assert.AreEqual(1, competitionsList.Count);
+            Assert.AreEqual("2018-12-25 Test Competition", competitionsList[0]);
+        }
+    }
+}
--- TPhotoCompetitionViewerTests/Competitions/ImagePathsTest.cs
+++ TPhotoCompetitionViewerTests/Competitions/ImagePathsTest.cs
@@ -1,40 +1,53 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using TPhotoCompetitionViewer.Competitions;
+using TPhotoCompetitionViewer.Properties;
 
 namespace TPhotoCompetitionViewerTests.Competitions
 {
     [TestClass]
     public class ImagePathsTest
     {
+        [TestInitialize]
+        public void Setup()
+        {
+            // Get hold of settings, then change them
+            Settings.Default.Reload();
+            var extractDir = Settings.Default.CompetitionExtractDir;
+            var srcDir = Settings.Default.CompetitionSrcDir;
+
+            Settings.Default.PropertyValues["CompetitionExtractDir"].PropertyValue = "./TestData";
+            Settings.Default.PropertyValues["CompetitionSrcDir"].PropertyValue = "./TestCompetitionZip";
+        }
+
         [TestMethod]
         public void TestGetExtractDirectory()
         {
             string competitionName = "2018-12-25 Test Competition";
             string extractDirectory = ImagePaths.GetExtractDirectory(competitionName);
 
-            Assert.AreEqual("c:/CompetitionsExtract/extract/2018-12-25 Test Competition/all", extractDirectory);
+            Assert.AreEqual("./TestData/extract/2018-12-25 Test Competition/all", extractDirectory);
         }
 
         [TestMethod]
         public void TestRemoveSourcePathStart()
         {
-            string extractDirectory = ImagePaths.RemoveSourcePathStart("c:/Competitions/extract/2018-12-25 Test Competition/all");
-            Assert.AreEqual("extract/2018-12-25 Test Competition/all", extractDirectory);
+            string extractDirectory = ImagePaths.RemoveSourcePathStart("./TestCompetitionZip/2018-12-25 Test Competition.zip");
+            Assert.AreEqual("2018-12-25 Test Competition.zip", extractDirectory);
         }
 
         [TestMethod]
         public void TestGetZipFile()
         {
             string extractDirectory = ImagePaths.GetZipFile("2018-12-25 Test Competition");
-            Assert.AreEqual("c:/Competitions/2018-12-25 Test Competition.zip", extractDirectory);
+            Assert.AreEqual("./TestCompetitionZip/2018-12-25 Test Competition.zip", extractDirectory);
         }
 
         [TestMethod]
         public void TestGetCompetitionsDirectory()
         {
             string directory = ImagePaths.GetCompetitionsDirectory();
-            Assert.AreEqual("c:/Competitions", directory);
+            Assert.AreEqual("./TestCompetitionZip", directory);
         }
 
         [TestMethod]
@@ -43,7 +56,7 @@ namespace TPhotoCompetitionViewerTests.Competitions
             string competitionName = "2018-12-25 Test Competition";
             string directory = ImagePaths.GetHeldDirectory(competitionName);
 
-            Assert.AreEqual("c:/CompetitionsExtract/extract/2018-12-25 Test Competition/held", directory);
+            Assert.AreEqual("./TestData/extract/2018-12-25 Test Competition/held", directory);
         }
 
         [TestMethod]
@@ -52,7 +65,7 @@ namespace TPhotoCompetitionViewerTests.Competitions
             string competitionName = "2018-12-25 Test Competition";
             string directory = ImagePaths.GetDatabaseDirectory(competitionName);
 
-            Assert.AreEqual("c:/CompetitionsExtract/extract/2018-12-25 Test Competition/db", directory);
+            Assert.AreEqual("./TestData/extract/2018-12-25 Test Competition/db", directory);
         }
 
         [TestMethod]
@@ -61,8 +74,7 @@ namespace TPhotoCompetitionViewerTests.Competitions
             string competitionName = "2018-12-25 Test Competition";
             string directory = ImagePaths.GetDatabaseFile(competitionName);
 
-            Assert.AreEqual("c:/CompetitionsExtract/extract/2018-12-25 Test Competition/db/2018-12-25 Test Competition.sqlite", directory);
+            Assert.AreEqual("./TestData/extract/2018-12-25 Test Competition/db/2018-12-25 Test Competition.sqlite", directory);
         }
-
     }
 }
